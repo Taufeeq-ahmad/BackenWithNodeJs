6df673a885da3e7d516abef1789cb545f2a5050c
Taufeeq ahmad <taufeeqahmad.cs@gmail.com>
2026-08-14 01:15:42 +0500

    SERVER CREATED

--- server.js
+++ server.js
@@ -2,9 +2,17 @@ const express = require('express');
 const app = express();
 
 // Middleware to parse JSON requests
-app.use(express.json());
+//app.use(express.json());
+
 
 // Sample route
 app.get('/', (req, res) => {
-    res.send('Hello, World!');
+    res.send('Hello, World! d');
+});
+app.get("/about", (req, res) => {
+    res.send('About Page');
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
 });
